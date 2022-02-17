977792e0c5cb908c21d70fb2dd24d2596c5f22c2
Eric Noubissi <noubissi10@yahoo.fr>
2022-02-17 15:23:41 +0100

    Correction OthersSkills.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,11 +1,63 @@
 import React from 'react';
 import Navigation from '../components/Navigation';
+import { CopyToClipboard } from 'react-copy-to-clipboard';
 
 const Contact = () => {
     return (
-        <div>
+        <div className="contact">
             <Navigation/>
-            Pour me contacter
+            <div className="contactContent">
+                <div className="header"></div>
+                <div className="contactBox">
+                    <h1>Contactez-moi</h1>
+                    <ul>
+                        <li>
+                            <i className="fas fa-map-marker-alt"></i>
+                            <span>3 rue Oberlin, <br /> 
+                                  67000 Strasbourg</span>
+                        </li>
+                        <li>
+                            <i className="fas fa-mobile-alt"></i>
+                            <CopyToClipboard text="0698342060">
+                                <span 
+                                    className="clickInput"
+                                    onClick={() => {alert ('Téléphone copié!');}}>
+                                        06 98 34 20 60
+                                </span>
+                            </CopyToClipboard>
+                        </li>
+                        <li>
+                            <i className="fas fa-envelope"></i>
+                            <CopyToClipboard text="rcnoubissi@gmail.com">
+                                <span 
+                                    className="clickInput"
+                                    onClick={() => {alert ('E-mail copié!');}}>
+                                        rcnoubissi@gmail.com
+                                </span>
+                            </CopyToClipboard>
+                        </li>
+                    </ul>
+                </div>
+                <div className="socialNetwork">
+                    <ul>
+                        <a href="https://www.linkedin.com/in/ecnoubissi/" 
+                            target="_blank" rel="noopener noreferrer">
+                            <h4>LinkedIn</h4>
+                            <i className="fab fa-linkedin"></i>
+                        </a>
+                        <a href="https://github.com/eric-colday" 
+                            target="_blank" rel="noopener noreferrer">
+                            <h4>Github</h4>
+                            <i className="fab fa-github"></i>
+                        </a>
+                        <a href="https://codepen.io/eric-colday-noubissi" 
+                            target="_blank" rel="noopener noreferrer">
+                            <h4>CodePen</h4>
+                            <i className="fab fa-codepen"></i>
+                        </a>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
